feb8ed4eed53a454b79bd665ae0d7b2aea8d4c2b
thatavarthi.vishn <thatavarthi.vishn@LP-PF2HJ6CL.HCLT.CORP.HCL.IN>
2021-12-30 12:59:20 +0530

    my first commit

---
+++ gitproject/src/test/java/samplegitproject/SampleGitProject.java
@@ -0,0 +1,13 @@
+package samplegitproject;
+
+import org.testng.annotations.Test;
+
+public class SampleGitProject {
+
+	@Test
+	public static void mtd1() {
+		// TODO Auto-generated method stub
+		System.out.println("git push");
+	}
+
+}
